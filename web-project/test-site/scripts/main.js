553f3173576dcac01c8ae67753a372092dde0ec9
yokota <ee217067@meiji.ac.jp>
2024-05-14 17:07:25 +0900

    JavaScriptの基本

---
+++ web-project/test-site/scripts/main.js
@@ -0,0 +1,33 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/fire-fox-icon.png") {
+    myImage.setAttribute("src", "images/fire-fox-icon2.png");
+  } else {
+    myImage.setAttribute("src", "images/fire-fox-icon.png");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("あなたの名前を入力してください。");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `${myName} さん、Mozilla はかっこいいよ。`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla はかっこいいよ、${storedName}`;
+}
+myButton.onclick = () => {
+    setUserName();
+};
